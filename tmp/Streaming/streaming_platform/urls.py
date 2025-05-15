442a824038c25ebc7e1e5fe064feb9e1bfc69e33
jonh-carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-05-15 20:21:25 -0300

    update django

--- tmp/Streaming/streaming_platform/urls.py
+++ tmp/Streaming/streaming_platform/urls.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.shortcuts import redirect
+
+
+# Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Conteúdos",
+        default_version='v1',
+        description="Documentação da API para o app de streaming de áudio e vídeo",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="suporte@exemplo.com"),
+        license=openapi.License(name="Licença BSD"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('content_app.urls')),
+
+    # Swagger e Redoc
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+  
 ]
